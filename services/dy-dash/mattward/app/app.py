6973ec195b23c1fa3d445887d4352de13c6a6385
odeimaiz <maiz@itis.swiss>
2019-05-29 11:59:00 +0200

    Building input data dynamically

--- services/dy-dash/mattward/app/app.py
+++ services/dy-dash/mattward/app/app.py
@@ -4,9 +4,17 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 
+import random
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+centered_text = {
+	'text-align': 'center'
+}
 tabs_styles = {
 	'height': '44px'
 }
@@ -21,76 +29,38 @@ tab_selected_style = {
 app.layout = html.Div(children=[
 	html.H1(
 		children='MattWard solver',
-		style={'text-align': 'center'}
+		style=centered_text
 	),
 
 	html.Div(
 		children='Minimal description of how the solver works.',
-		style={'textAlign': 'center'}
+		style=centered_text
 	),
 
 	html.Div([
 		# Four input graphs on the left
 		html.Div([
-			html.H4('Inputs'),
-
-			dcc.Graph(
-				id='graph-in1',
-				figure={
-					'data': [
-						{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-						{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-					],
-					'layout': {
-						'title': 'Graph input 1'
-					}
-				}
+			html.H4(
+				children='Learned Model Input Parameters',
+				style=centered_text
 			),
+			
+			# Hidden div inside the app that stores the input data
+			html.Div(id='input-data', style={'display': 'none'}),
 
-			dcc.Graph(
-				id='graph-in2',
-				figure={
-					'data': [
-						{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-						{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-					],
-					'layout': {
-						'title': 'Graph input 2'
-					}
-				}
-			),
-
-			dcc.Graph(
-				id='graph-in3',
-				figure={
-					'data': [
-						{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-						{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-					],
-					'layout': {
-						'title': 'Graph input 3'
-					}
-				}
-			),
-
-			dcc.Graph(
-				id='graph-in4',
-				figure={
-					'data': [
-						{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-						{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-					],
-					'layout': {
-						'title': 'Graph input 4'
-					}
-				}
-			)
-		], style={'width': '42%', 'float': 'left'}),
+			dcc.Graph(id='graph-in1'),
+			dcc.Graph(id='graph-in2'),
+			dcc.Graph(id='graph-in3'),
+			dcc.Graph(id='graph-in4')
+		], style={'width': '41%', 'float': 'left'}),
 
 
 		# Controls in the middle
 		html.Div([
-			html.H4('Controls'),
+			html.H4(
+				children='Controls',
+				style=centered_text
+			),
 
 			html.Div([
 				html.H5('Input options'),
@@ -132,11 +102,14 @@ app.layout = html.Div(children=[
 				),
 				html.Div(id='tabs-content')
 			], style={'border': '1px solid', 'border-radius': '5px'})
-		], style={'width': '15%', 'float': 'left', 'max-width': '340px', 'min-width': '210px'}),
+		], style={'width': '15%', 'float': 'left', 'max-width': '340px', 'min-width': '220px'}),
 
 		# Two output graphs on the right
 		html.Div([
-			html.H4('Outputs'),
+			html.H4(
+				children='Outputs',
+				style=centered_text
+			),
 
 			dcc.Graph(
 				id='graph-out1',
@@ -163,9 +136,120 @@ app.layout = html.Div(children=[
 					}
 				}
 			)
-		], style={'width': '42%', 'float': 'left'}),
+		], style={'width': '41%', 'float': 'left'}),
 	], style={'margin': '5px 0'})
 ])
+
+# When pressing 'Load' this callback will be triggered.
+# Also, its output will trigger the rebuilding of the four input graphs.
+@app.callback(
+	Output('input-data', 'children'),
+	[Input('load-input-button', 'n_clicks')]
+)
+def read_input_file(n_clicks):
+	# column_names = ['t_ms', 'CV', 'Vmax','M_mod', 'B_mod', 'tauSD']
+	# data = pd.read_csv(path, sep=',', names=column_names)
+	# return [random.randint(1,10), random.randint(1,10), random.randint(1,10)]
+	return {
+		"x_axis": [1, 2, 3, 4],
+		"y_axis": {
+			"Vmax": [random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)],
+			"M_mod": [random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)],
+			"B_mod": [random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)],
+			"tauSD": [random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)],
+		}
+	}
+
+
+@app.callback(
+	Output('graph-in1', 'figure'),
+	[Input('input-data', 'children')]
+)
+def build_graph_1(data):
+	return {
+		'layout': {
+			'title': 'Vmax(uV)'
+		},
+		'data': [
+			go.Scatter(
+				x=data["x_axis"],
+				y=data["y_axis"]["Vmax"],
+				mode='lines+markers'
+			)
+		]
+	}
+
+
+@app.callback(
+	Output('graph-in2', 'figure'),
+	[Input('input-data', 'children')]
+)
+def build_graph_2(data):
+	return {
+		'layout': {
+			'title': 'M coeff'
+		},
+		'data': [
+			go.Scatter(
+				x=data["x_axis"],
+				y=data["y_axis"]["M_mod"],
+				mode='lines+markers'
+			)
+		]
+	}
+
+
+@app.callback(
+	Output('graph-in3', 'figure'),
+	[Input('input-data', 'children')]
+)
+def build_graph_3(data):
+	return {
+		'layout': {
+			'title': 'B coeff (mA)'
+		},
+		'data': [
+			go.Scatter(
+				x=data["x_axis"],
+				y=data["y_axis"]["B_mod"],
+				mode='lines+markers'
+			)
+		]
+	}
+
+
+@app.callback(
+	Output('graph-in4', 'figure'),
+	[Input('input-data', 'children')]
+)
+def build_graph_4(data):
+	return {
+		'layout': {
+			'title': 'tau_SD(ms)'
+		},
+		'data': [
+			go.Scatter(
+				x=data["x_axis"],
+				y=data["y_axis"]["tauSD"],
+				mode='lines+markers'
+			)
+		]
+	}
+
+
+@app.callback(
+	Output('output-container-button', 'children'),
+	[Input('load-input-button', 'n_clicks')],
+	state=[
+		State(component_id='input-nerve-profile', component_property='value'),
+		State(component_id='input-plot-options', component_property='values')
+	]
+)
+def load_input(n_clicks, input_nerve_profile, input_plot_options):
+	return 'Input options: {} in1 {} in2'.format(
+		input_nerve_profile,
+		input_plot_options
+	)
 
 
 @app.callback(Output('tabs-content', 'children'),
@@ -215,20 +299,6 @@ def render_content(tab):
 			html.H3('Duration props')
 		])
 
-
-@app.callback(
-	Output('output-container-button', 'children'),
-	[Input('load-input-button', 'n_clicks')],
-	state=[
-		State(component_id='input-nerve-profile', component_property='value'),
-		State(component_id='input-plot-options', component_property='values')
-	]
-)
-def load_input(n_clicks, input_nerve_profile, input_plot_options):
-	return 'Input options: {} in1 {} in2'.format(
-		input_nerve_profile,
-		input_plot_options
-	)
 
 
 if __name__ == '__main__':
