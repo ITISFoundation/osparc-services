c8412e64604b458f42497d55a6104a3d2696f251
Manuel Guidon <guidon@itis.swiss>
2019-06-06 16:30:13 +0200

    Adds examples and the actual software

---
+++ services/dy-jupyter-extensions/neuron/docker/boot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# BOOTING application ---------------------------------------------
+echo "Booting application ..."
+echo "  User    :`id $(whoami)`"
+echo "  Workdir :`pwd`"
+
+## if test "${CREATE_DUMMY_TABLE}" = "1"
+## then
+##     # pushd /home/jovyan/packages/simcore-sdk; pip install -r requirements-dev.txt; popd
+## 
+##     echo "Creating dummy tables ... using ${USE_CASE_CONFIG_FILE}"
+##     result="$(python scripts/dy_services_helpers/platform_initialiser_csv_files.py ${USE_CASE_CONFIG_FILE} ${INIT_OPTIONS})"
+##     echo "Received result of $result";
+##     IFS=, read -a array <<< "$result";
+##     echo "Received result pipeline id of ${array[0]}";
+##     echo "Received result node uuid of ${array[1]}";
+##     # the fake SIMCORE_NODE_UUID is exported to be available to the service
+##     export SIMCORE_PROJECT_ID="${array[0]}";
+##     export SIMCORE_NODE_UUID="${array[1]}";
+## fi
+
+# start the notebook now
+echo "Setting theme ..."
+jt -t oceans16 -f ubuntu -fs 12 -T -N -cellw 90%
+/docker/boot_notebook.sh #--NotebookApp.default_url=/notebooks/${NOTEBOOK_URL}
---
+++ services/dy-jupyter-extensions/neuron/notebooks/examples/netpyne/HHTut/HHTut_export.py
@@ -0,0 +1,10 @@
+import HHTut  # import parameters file
+from netpyne import sim  # import netpyne sim module
+
+np = HHTut.netParams
+print("********************\n*\n*  Note: setting noise to 1, since noise can only be 0 or 1 in NeuroML export currently!\n*\n********************")
+np.stimSourceParams['bkg']['noise'] = 1
+
+sim.createExportNeuroML2(netParams = np, 
+                       simConfig = HHTut.simConfig,
+                       reference = 'HHTut')  # create and export network to NeuroML 2
