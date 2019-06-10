687c26e274035349013a74377fac7bc03012b148
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-06-10 23:16:07 +0200

    renamed folders. should use an env

--- services/dy-3dvis/simcoreparaviewweb/docker/visualizer_launcher_patch.sh
+++ services/dy-3dvis/simcoreparaviewweb/docker/visualizer_launcher_patch.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 sed -i -e '/"--authKey", "${secret}",/a "--settings-lod-threshold", "5", "--no-built-in-palette", "--color-palette-file", "/home/root/config/s4lColorMap.json",' /opt/wslink-launcher/launcher-template.json
-
-if [ -d "/data/server-state" ] && [ -f "/data/server-state/savedState.pvsm" ]; then
-    # modify apache configuration
-    sed -i -e '/"--authKey", "${secret}",/a "--load-file", "server-state/savedState.pvsm",' /opt/wslink-launcher/launcher-template.json
+#TODO: use an ENV here
+if [ -d "/data/save-data" ] && [ -f "/data/save-data/state.pvsm" ]; then
+    # modify launcher configuration
+    sed -i -e '/"--authKey", "${secret}",/a "--load-file", "save-data/state.pvsm",' /opt/wslink-launcher/launcher-template.json
 fi
