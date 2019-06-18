1fe33e33a3458de942e80f0e14372efbfea2b70f
Sylvain <35365065+sanderegg@users.noreply.github.com>
2019-06-18 21:01:51 +0200

    Is779/paraview persistency (#4)
    Switched multi-user framework to single-user
    Keep status when refreshing client
    Save state when calling POST state received
    Restore state when calling GET state
    Added GPU version

--- services/dy-3dvis/simcoreparaviewweb/docker/boot.sh
+++ services/dy-3dvis/simcoreparaviewweb/docker/boot.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+echo
 echo "current directory is ${PWD}"
 
-# we need to be in python 3.6 to install simcore stuff
+echo
+echo "set up python 3"
 export PATH="${PYENV_ROOT}/bin:$PATH"
 eval "$(pyenv init -)"
 python -V
@@ -9,54 +15,73 @@ pip -V
 
 if [[ -v CREATE_DUMMY_TABLE ]];
 then
-    pushd /home/root/packages/simcore-sdk; pip3 install -r requirements-dev.txt; popd
-    pushd /home/root/packages/s3wrapper; pip3 install -r requirements-dev.txt; popd
     pushd /home/root/scripts/dy_services_helpers; pip3 install -r requirements.txt; popd
     # in dev mode, data located in mounted volume /test-data are uploaded to the S3 server
     # also a fake configuration is set in the DB to simulate the osparc platform
-    echo "development mode, creating dummy tables..."
+    echo
+    echo "development mode, init dummy pipeline..."
     # in style: pipelineid,nodeuuid
     result="$(python3 scripts/dy_services_helpers/platform_initialiser.py ${USE_CASE_CONFIG_FILE} --folder ${TEST_DATA_PATH})";
     echo "Received result of $result";
-    IFS=, read -a array <<< "$result"; 
+    IFS=, read -a array <<< "$result";
     echo "Received result pipeline id of ${array[0]}";
     echo "Received result node uuid of ${array[1]}";
     # the fake SIMCORE_NODE_UUID is exported to be available to the service
     export SIMCORE_NODE_UUID="${array[1]}";
+    export SIMCORE_PROJECT_ID="${array[0]}"
+    # we need to copy the handlers in the correct folder (only for debug mode to prevent masking files)
+    cp handlers/*.rpy /opt/paraview/share/paraview-5.6/web/visualizer/www/
 fi
 
-echo "modifying apache configuration..."
-. docker/apachePatch.sh
+# try to pull data from S3
+echo
+echo "trying to download previous state..."
+python docker/state_manager.py pull --path ${SIMCORE_NODE_APP_STATE_PATH} --silent
+echo "...DONE"
+echo
 
-echo "restarting the apache service..."
-service apache2 restart
+# patch paraview
+echo
+echo "patching paraviewweb to allow for rpy scripts to run"
+docker/patch_paraview.sh
 
-# echo "modifying wslink launcher configuration"
-. docker/visualizer_launcher_patch.sh
-
-if [[ -v CREATE_DUMMY_TABLE ]];
-then
-    # in dev mode we know already what the host/port are
-    host_name=${HOST_NAME}
-    server_port=${SERVER_PORT}    
-else
-    # the service waits until the calling client transfers the service externally published port/hostname
-    # this is currently necessary due to some unknown reason with regard to how paraviewweb 
-    # visualizer is started (see below)
-    echo "Waiting for server hostname/port to be defined"
-    host_port="$(python docker/getport.py)";
-    echo "Received hostname/port: ${host_port}"
-    IFS=, read -a array <<< "$host_port"; 
-    echo "Host name decoded as ${array[0]}";
-    echo "Port decoded as ${array[1]}";
-    host_name=${array[0]}
-    server_port=${array[1]}
-fi
-
-
-# to start the paraviewweb visualizer it needs as parameter something to do with the way
-# its websockets are setup "ws://HOSTNAME:PORT" hostname and port must be the hostname and port
-# as seen from the client side (if in local development mode, this would be typically localhost and 
+# the paraviewweb visualizer needs as host parameter the final endpoint of the client to establish the
+# websocket. the endpoint must be the hostname and port
+# as seen from the client side (if in local development mode, this would be typically localhost and
 # whatever port is being published outside the docker container)
-echo "starting paraview using hostname ${host_name} and websocket port $server_port..."
-/opt/paraviewweb/scripts/start.sh "ws://${host_name}:$server_port"
+
+# move base path to expected node base path (sic)
+mkdir -p /opt/paraview/share/paraview-5.6/web/visualizer/www${SIMCORE_NODE_BASEPATH}
+mv /opt/paraview/share/paraview-5.6/web/visualizer/www/*.* /opt/paraview/share/paraview-5.6/web/visualizer/www${SIMCORE_NODE_BASEPATH}
+
+# set default parameters (note that port is the server local port, and host is used for the websocket location)
+visualizer_options=(--content /opt/paraview/share/paraview-5.6/web/visualizer/www/ \
+                    --data ${PARAVIEW_INPUT_PATH} \
+                    --host ${SIMCORE_HOST_NAME}:${SERVER_PORT}${SIMCORE_NODE_BASEPATH} \
+                    --port ${SERVER_PORT} \
+                    --timeout 20000 \
+                    --no-built-in-palette \
+                    --color-palette-file /home/root/config/s4lColorMap.json \
+                    --settings-lod-threshold 5
+                    )
+
+# set auto load state if available
+if [ -f "${PARAVIEW_INPUT_PATH}/${SIMCORE_STATE_FILE}" ]; then
+    echo
+    echo "setting autoload of ${SIMCORE_STATE_FILE}"
+    visualizer_options+=(--load-file ${SIMCORE_STATE_FILE})
+fi
+
+# show additional debugging parameters on demand
+if [[ ${PARAVIEW_DEBUG} != 0 ]]; then
+    echo
+    echo "setting paraview debug mode on"
+    visualizer_options+=(--debug)
+fi
+
+# start server
+echo
+echo "starting paraview on ${SIMCORE_HOST_NAME}:${SERVER_PORT}${SIMCORE_NODE_BASEPATH}..."
+echo "using ${visualizer_options[@]}"
+/opt/paraview/bin/pvpython -dr --mpi \
+    /opt/paraview/share/paraview-5.6/web/visualizer/server/pvw-visualizer.py "${visualizer_options[@]}"
