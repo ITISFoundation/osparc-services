af208cdc155926fa1358fc7c2a45a7a2998d695e
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-06-24 10:58:04 +0200

    fix image name

--- services/dy-dash/mattward-dash/tests/integration/test_docker_image.py
+++ services/dy-dash/mattward-dash/tests/integration/test_docker_image.py
@@ -25,7 +25,9 @@ def docker_client() -> docker.DockerClient:
 
 @pytest.fixture
 def docker_image_key(docker_client: docker.DockerClient) -> str:
-    image_key = "simcore/services/comp/mattward-dash:latest"
+    docker_registry = os.environ.get("DOCKER_REGISTRY", "itisfoundation")
+    docker_image_tag = os.environ.get("DOCKER_IMAGE_TAG", "latest")
+    image_key = "{}/mattward-dash:{}".format(docker_registry, docker_image_tag)
     docker_images = [image for image in docker_client.images.list() if any(image_key in tag for tag in image.tags)]
     return docker_images[0].tags[0]
 
