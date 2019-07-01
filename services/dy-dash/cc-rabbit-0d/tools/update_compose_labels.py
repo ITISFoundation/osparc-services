494da709d4527e6e3418aceb8954ede6e43f1b30
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2019-07-01 20:19:31 +0200

    Add CC-Rabbit-0D and CC-Rabbit-1D services (#8)
    * got rid of the scroll bars when loading
    * create_graphs implemented
    * testing data added
    * cc-rabbit-0d with cookiecutter
    * travis.sh added
    * port_config added
    * cc-0d-dash -> cc-rabbit-0d
    * comp -> dynamic
    * inputs updated
    * outputs updated
    * computational -> dynamic
    * Updating boot.sh + added healthcheck
    * pylint to test requirements
    * using jupyter/base-notebook instead of cnap
    * make build updates files
    * @sanderegg makes changes remotely
    * docker-composes updated
    * Update .env-devel
    * mount input folder
    * DEVEL_MODE = False
    * all plots there
    * Delete .gitkeep
    * Delete HOWTOBAKE.md
    * coockiecutter produces cc-rabbit-1d
    * move to dy-dash
    * cc-rabbit-1d app added
    * Add cookiecutter missing stuff
    * input/ouput labels updated
    * add mockup inputs
    * application ready
    * env-devel added
    * "make build" result
    * docker-compose files updated
    * Update Makefile
    * brighter colormap
    * Bump version: 0.1.0 → 1.0.0
    * @sanderegg reviews
    * run_creator not needed
    * Update test_folder_structure.py

---
+++ services/dy-dash/cc-rabbit-0d/tools/update_compose_labels.py
@@ -0,0 +1,82 @@
+#!/bin/python
+
+""" Update a docker-compose file with json files in a path
+
+    Usage: python update_compose_labels --c docker-compose.yml -f folder/path
+
+:return: error code
+"""
+
+import argparse
+import json
+import logging
+import sys
+from enum import IntEnum
+from pathlib import Path
+from typing import Dict
+
+import yaml
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+class ExitCode(IntEnum):
+    SUCCESS = 0
+    FAIL = 1
+
+def get_compose_file(compose_file: Path) -> Dict:
+    with compose_file.open() as filep:
+        return yaml.safe_load(filep)
+
+
+def stringify_file(input_file: Path) -> str:
+    with input_file.open() as filep:
+        return json.dumps(json.load(filep))
+
+
+def stringify_folder(folder: Path) -> Dict:
+    jsons = {}
+    for json_file in folder.glob("*.json"):
+        jsons["io.simcore.{}".format(json_file.stem)] = stringify_file(json_file)
+    return jsons
+
+
+def update_compose_labels(compose_cfg: Dict, json_labels: Dict) -> bool:
+    compose_labels = compose_cfg["services"]["cc-rabbit-0d"]["build"]["labels"]
+    changed = False
+    for json_key, json_value in json_labels.items():
+        if json_key in compose_labels:
+            if compose_labels[json_key] == json_value:
+                continue
+        compose_labels[json_key] = json_value
+        changed = True
+    return changed
+
+def main(args = None) -> int:
+    try:
+        parser = argparse.ArgumentParser(description=__doc__)
+        parser.add_argument("--compose", help="The compose file where labels shall be updated", type=Path, required=True)
+        parser.add_argument("--input", help="The json folder to stringify", type=Path, required=True)
+        options = parser.parse_args(args)
+
+
+        log.info("Testing if %s needs updates using labels in %s", options.compose, options.input)
+        # get available jsons
+        compose_cfg = get_compose_file(options.compose)
+        json_labels = stringify_folder(options.input)
+        if update_compose_labels(compose_cfg, json_labels):
+            log.info("Updating %s using labels in %s", options.compose, options.input)
+            # write the file back
+            with options.compose.open('w') as fp:
+                yaml.safe_dump(compose_cfg, fp, default_flow_style=False)
+                log.info("Update completed")
+        else:
+            log.info("No update necessary")
+        return ExitCode.SUCCESS
+    except: #pylint: disable=bare-except
+        log.exception("Unexpected error:")
+        return ExitCode.FAIL
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ services/dy-dash/cc-rabbit-1d/src/cc-rabbit-1d/cc-rabbit-1d.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+import asyncio
+
+import numpy as np
+import pandas as pd
+import flask
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+import plotly.graph_objs as go
+from plotly import tools
+
+
+DEVEL_MODE = False
+if DEVEL_MODE:
+    WORKDIR = Path(Path(os.path.dirname(os.path.realpath(__file__))).parent).parent
+else:
+    WORKDIR = Path('/home/jovyan')
+INPUT_DIR = WORKDIR / 'input'
+
+
+base_pathname = os.environ.get('SIMCORE_NODE_BASEPATH', "/")
+if not base_pathname.endswith("/"):
+    base_pathname = base_pathname + "/"
+if not base_pathname.startswith("/"):
+    base_pathname = "/" + base_pathname
+print('url_base_pathname', base_pathname)
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__,
+    server=server,
+    url_base_pathname=base_pathname
+)
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
+
+
+osparc_style = {
+    'color': '#bfbfbf',
+    'backgroundColor': '#202020',
+    'gridColor': '#444',
+}
+dcc_input = {
+    'color': osparc_style['color'],
+    'backgroundColor': osparc_style['gridColor']
+}
+dcc_input_button = {
+    'height': '40px',
+    'width': '100%',
+    'color': dcc_input['color'],
+    'backgroundColor': dcc_input['backgroundColor']
+}
+
+GRAPH_HEIGHT = 400
+
+def give_fig_osparc_style2(fig):
+    margin = 10
+    y_label_padding = 50
+    x_label_padding = 30
+    fig['layout']['margin'].update(
+        l=margin+y_label_padding,
+        r=margin,
+        b=margin+x_label_padding,
+        t=margin,
+    )
+
+    fig['layout'].update(
+        autosize=True,
+        height=GRAPH_HEIGHT,
+        showlegend=False,
+        plot_bgcolor=osparc_style['backgroundColor'],
+        paper_bgcolor=osparc_style['backgroundColor'],
+        font=dict(
+            color=osparc_style['color']
+        )
+    )
+    return fig
+
+def give_fig_osparc_style(fig, xLabels=['x'], yLabels=['y']):
+    for idx, xLabel in enumerate(xLabels):
+        suffix = str(idx)
+        if idx is 0:
+            suffix = ''
+        fig['layout']['xaxis'+suffix].update(
+            title=xLabel,
+            gridcolor=osparc_style['gridColor']
+        )
+    for idx, yLabel in enumerate(yLabels):
+        suffix = str(idx)
+        if idx is 0:
+            suffix = ''
+        fig['layout']['yaxis'+suffix].update(
+            title=yLabel,
+            gridcolor=osparc_style['gridColor']
+        )
+    fig = give_fig_osparc_style2(fig)
+    return fig
+
+def get_empty_graph(xLabel='x', yLabel='y'):
+    fig = go.Figure(data=[], layout={})
+    fig = give_fig_osparc_style(fig, [xLabel], [yLabel])
+    return fig
+
+
+empty_graph_1 = get_empty_graph()
+empty_graph_2 = get_empty_graph()
+
+app.layout = html.Div(children=[
+    html.Button('Reload', id='reload-button', style=dcc_input_button),
+    dcc.Graph(id='graph-1', figure=empty_graph_1),
+    dcc.Graph(id='graph-2', figure=empty_graph_2)
+], style=osparc_style)
+
+
+def create_graph(data_frame, title=None, x_axis_title=None, y_axis_title=None):
+    data = [
+            go.Scatter(
+                x=data_frame[data_frame.columns[0]],
+                y=data_frame[data_frame.columns[i]],
+                name=str(data_frame.columns[i])
+            )
+            for i in range(1,data_frame.columns.size)
+    ]
+
+    fig = get_empty_graph(x_axis_title, y_axis_title)
+    layout = go.Layout(fig['layout'])
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+def plot_ECG():
+    # data_path_a = await PORTS.inputs[0].get()
+    data_path_a = INPUT_DIR / 'ECGs.txt'
+    data_frame_a = pd.read_csv(data_path_a, sep="\t", header=None)
+
+    axis_colums = [0,1]
+    plot_1 = data_frame_a.filter(items=[data_frame_a.columns[i] for i in axis_colums])
+    fig = create_graph(data_frame=plot_1, x_axis_title='Time (sec)', y_axis_title='ECGs')
+    return fig
+
+def ap_surface_1D():
+    # data_path_str = await PORTS.inputs[1].get()
+    data_path_str = INPUT_DIR / 'y_1D.txt'
+    data_frame_JJ = pd.read_csv(data_path_str, sep="\t", header=None)
+    out_v = data_frame_JJ
+
+    num_cells = 165
+    # point ranges you want to plot
+    min = 0 #90000;
+    max = out_v.shape[0]-1 # 4 #100000;
+    diff = (max - min + 1)
+
+    t = np.array(out_v.iloc[range(min,max+1), 0]).reshape(diff, 1)
+    T = t
+    #creates a matrix of t matricies (1 for each cell)
+    for b in range(1,(num_cells)):
+        T = np.hstack((T, t))
+
+    #creats an array of cell numbers
+    cellnum = np.array([x for x in range(1, num_cells+1)]).reshape(1, num_cells)
+
+    #creates a square matrix of cell numbers on the diagonal
+    cellm = np.ones((diff,1)) @ cellnum
+    vm = out_v.iloc[range(min,max+1), range(1,num_cells+1)]
+
+    colormap = [
+        [0, 'rgb(40.0, 40.0, 40.0)'],
+        [1.0, 'rgb(240.0, 240.0, 240.0)']
+    ]
+
+    data = [
+        go.Surface(
+            x=cellm,
+            y=T,
+            z=vm.values,
+            colorscale=colormap,
+            reversescale=True
+        )
+    ]
+
+    camera = dict(
+        up=dict(x=-1, y=0, z=0),
+        center=dict(x=0,y=0,z=0),
+        eye=dict(x=1, y=0, z=2)
+    )
+
+    layout = go.Layout(
+        title=None,
+        scene=dict(camera=camera),
+    )
+    fig = go.Figure(data=data, layout=layout)
+    fig = give_fig_osparc_style2(fig)
+    return fig
+
+# When pressing 'Load' this callback will be triggered.
+# Also, its output will trigger the rebuilding of the four input graphs.
+@app.callback(
+    [
+        Output('graph-1', 'figure'),
+        Output('graph-2', 'figure')
+    ],
+    [
+        Input('reload-button', 'n_clicks')
+    ]
+)
+def read_input_files(_n_clicks):
+    figs = [
+        plot_ECG(),
+        ap_surface_1D()
+    ]
+    return figs
+
+
+class AnyThreadEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+    """Event loop policy that allows loop creation on any thread."""
+
+    def get_event_loop(self) -> asyncio.AbstractEventLoop:
+        try:
+            return super().get_event_loop()
+        except (RuntimeError, AssertionError):
+            # "There is no current event loop in thread %r"
+            loop = self.new_event_loop()
+            self.set_event_loop(loop)
+            return loop
+
+if __name__ == '__main__':
+    # the following line is needed for async calls
+    asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+    app.run_server(debug=DEVEL_MODE, port=8888, host="0.0.0.0")
