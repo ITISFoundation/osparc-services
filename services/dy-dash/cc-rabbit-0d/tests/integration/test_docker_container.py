e5143acc1b069eb9b0bde8bef989798aa7a541c7
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2019-07-04 09:30:40 +0200

    Add Mattward-dash service (#2)
    * building layout
    * tabs styled
    * Building input data dynamically
    * Input plots subplotted (sharing x axis)
    * options side by side
    * 3d plot added
    * heatmap added
    * Wireframe plot instead of the 3D
    * fitting output plots
    * added separated css
    * hide undo/redo buttons
    * responsive layouut
    * all callback function ready
    * input sample added
    * reading input.csv file
    * dynamic output title depending on pulse sweeping option
    * use index values in dropdown menu
    * all info for matlab ready
    * input file moved
    * calling subprocess
    * Added dynamic-service folcer structure (copy&paste from dy-2Dgraph)
    * write output files in old mattward
    * making progress with the subprocess calls
    * call matlab for creating input.csv
    * enable admin in dy-dash use cases
    * added run_cnap method
    * first case working
    * all graphs correct!
    * Show message is application is busy
    * rename css file
    * push csv file to s3
    * n_neurons disabled
    * Dockerfile and requirements added
    * mattward-dash to dynamic-service
    * create an output folder
    * working with SIMCORE_NODE_BASEPATH
    * Travis friendly service key
    * service.cli away
    * Bump version: 0.1.3 → 0.1.4
    * dash uses its own helathchecker
    * make mattward faster by removing the hidden step
    * remove title in dash apps
    * travis calls for auto build
    * added integration tests in travis
    * fixes integration test 'simple'
    * fix image name
    * do not check output for dynamic service
    * travis script path updated
    * base image in itisfoundation
    * install test requirements
    * added pylint to requiremetns
    * added pylint to tests/requirements
    * Bump version: 0.2.0 → 1.0.0
    * run_creator deleted, test_folder_structure updated
    * after comparing to cc-rabbits
    * added cc-rabbit-0d and cc-rabbit-1d to travis testing
    * test now check whether service is started
    * @sanderegg's patch also for rabbits
    * fileToKeyMap added to labels
    * inputs moved to validation/input
    * outputs added to validation
    * 1d output added and labels updated
    * inputs/outputs moved to validation
    * 0d outputs added

--- services/dy-dash/cc-rabbit-0d/tests/integration/test_docker_container.py
+++ services/dy-dash/cc-rabbit-0d/tests/integration/test_docker_container.py
@@ -8,6 +8,7 @@ import filecmp
 import json
 import os
 import shutil
+import time
 from pathlib import Path
 from pprint import pformat
 from typing import Dict
@@ -24,69 +25,36 @@ def docker_client() -> docker.DockerClient:
 
 @pytest.fixture
 def docker_image_key(docker_client: docker.DockerClient) -> str:
-    image_key = "simcore/services/dynamic/cc-rabbit-0d:latest"
+    registry = os.environ.get("DOCKER_REGISTRY", "itisfoundation")
+    tag = os.environ.get("DOCKER_IMAGE_TAG", "latest")
+    image_key = "{}/cc-rabbit-0d:{}".format(registry, tag)
     docker_images = [image for image in docker_client.images.list() if any(image_key in tag for tag in image.tags)]
+    assert len(docker_images) == 1, "could not find docker image {}".format(image_key)
     return docker_images[0].tags[0]
 
-def _is_gitlab_executor() -> bool:
-    return "GITLAB_CI" in os.environ
+@pytest.fixture
+def dynamic_service_environ(env_devel: Dict):
+    for key, value in env_devel.items():
+        os.environ[key] = value
+
 
 @pytest.fixture
-def temporary_path(tmp_path: Path) -> Path:
-    if _is_gitlab_executor():
-        # /builds is a path that is shared between the docker in docker container and the job builder container
-        shared_path = Path("/builds/{}/tmp".format(os.environ["CI_PROJECT_PATH"]))
-        shared_path.mkdir(parents=True, exist_ok=True)
-        return shared_path
-    return tmp_path
-
-@pytest.fixture
-def host_folders(temporary_path: Path) -> Dict:
-    tmp_dir = temporary_path
-
-    host_folders = {}
-    for folder in _FOLDER_NAMES:
-        path = tmp_dir / folder
-        if path.exists():
-            shutil.rmtree(path)
-        path.mkdir()
-        # we need to ensure the path is writable for the docker container (Gitlab-CI case)
-        os.chmod(str(path), 0o775)
-        assert path.exists()
-        host_folders[folder] = path
-
-    return host_folders
-
-@pytest.fixture
-def container_variables() -> Dict:
-    # of type INPUT_FOLDER=/home/scu/data/input
-    env = {"{}_FOLDER".format(str(folder).upper()):(_CONTAINER_FOLDER / folder).as_posix() for folder in _FOLDER_NAMES}
-    return env
-
-@pytest.fixture
-def validation_folders(validation_dir: Path) -> Dict:
-    return {folder:(validation_dir / folder) for folder in _FOLDER_NAMES}
-
-@pytest.fixture
-def docker_container(validation_folders: Dict, host_folders: Dict, docker_client: docker.DockerClient, docker_image_key: str, container_variables: Dict) -> docker.models.containers.Container:
-    # copy files to input folder, copytree needs to not have the input folder around.
-    host_folders["input"].rmdir()
-    shutil.copytree(validation_folders["input"], host_folders["input"])
-    assert Path(host_folders["input"]).exists()
+def docker_container(dynamic_service_environ, docker_client: docker.DockerClient, docker_image_key: str) -> docker.models.containers.Container:
     # run the container (this may take some time)
     try:
-        volumes = {host_folders[folder]:{"bind":container_variables["{}_FOLDER".format(str(folder).upper())]} for folder in _FOLDER_NAMES}
-        container = docker_client.containers.run(docker_image_key,
-            "run", detach=True, remove=False, volumes=volumes, environment=container_variables)
-        response = container.wait()
-        if response["StatusCode"] > 0:
-            logs = container.logs(timestamps=True)
-            pytest.fail("The container stopped with exit code {}\n\n\ncommand:\n {}, \n\n\nlog:\n{}".format(response["StatusCode"],
-            "run", pformat(
-                (container.logs(timestamps=True).decode("UTF-8")).split("\n"), width=200
-                )))
-        else:
-            yield container
+        container_start_time = time.perf_counter()
+        container = docker_client.containers.run(docker_image_key, detach=True, remove=False, init=True)
+        while not container.status == "running":
+            if container.status == "exited":
+                # not good for a dynamic container
+                pytest.fail("The container exited already.\nlogs: {}".format(pformat(container.logs(timestamps=True).decode("UTF-8")).split("\n"), width=200))
+            # wait
+            time.sleep(1)
+            container.reload()
+        container_started_time = time.perf_counter()
+        # should be less than 10sec
+        assert (container_started_time - container_start_time) < 10
+        yield container
     except docker.errors.ContainerError as exc:
         # the container did not run correctly
         pytest.fail("The container stopped with exit code {}\n\n\ncommand:\n {}, \n\n\nlog:\n{}".format(exc.exit_status,
@@ -95,6 +63,7 @@ def docker_container(validation_folders: Dict, host_folders: Dict, docker_client
                 )))
     finally:
         #cleanup
+        container.stop()
         container.remove()
 
 def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
@@ -110,47 +79,5 @@ def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
     assert len(io_simcore_labels) > 0
     return io_simcore_labels
 
-def test_run_container(validation_folders: Dict, host_folders: Dict, docker_container: docker.models.containers.Container):
-    for folder in _FOLDER_NAMES:
-        # test if the files that should be there are actually there and correct
-        list_of_files = [x.name for x in validation_folders[folder].iterdir() if not ".gitkeep" in x.name]
-        for file_name in list_of_files:
-            assert Path(host_folders[folder] / file_name).exists(), "missing files in {}".format(host_folders[folder])
-        match, mismatch, errors = filecmp.cmpfiles(host_folders[folder], validation_folders[folder], list_of_files, shallow=False)
-        # assert not mismatch, "wrong/incorrect files in {}".format(host_folders[folder])
-        assert not errors, "missing files in {}".format(host_folders[folder])
-        # test if the files that are there are matching the ones that should be
-        if folder != "input":
-            list_of_files = [x.name for x in host_folders[folder].iterdir()  if not ".gitkeep" in x.name]
-            for file_name in list_of_files:
-                assert Path(validation_folders[folder] / file_name).exists(), "{} is not present in {}".format(file_name, validation_folders[folder])
-            match, mismatch, errors = filecmp.cmpfiles(host_folders[folder], validation_folders[folder], list_of_files, shallow=False)
-            # assert not mismatch, "wrong/incorrect generated files in {}".format(host_folders[folder])
-            assert not errors, "too many files in {}".format(host_folders[folder])
-
-    # check the output is correct based on container labels
-    output_cfg = {}
-    output_cfg_file = Path(host_folders["output"] / "output.json")
-    if output_cfg_file.exists():
-        with output_cfg_file.open() as fp:
-            output_cfg = json.load(fp)
-
+def test_run_container(docker_container: docker.models.containers.Container):
     container_labels = docker_container.labels
-    io_simcore_labels = _convert_to_simcore_labels(container_labels)
-    assert "outputs" in io_simcore_labels
-    for key, value in io_simcore_labels["outputs"].items():
-        assert "type" in value
-        # rationale: files are on their own and other types are in input.json
-        if not "data:" in value["type"]:
-            # check that keys are available
-            assert key in output_cfg
-        else:
-            # it's a file and it should be in the folder as well using key as the filename
-            filename_to_look_for = key
-            if "fileToKeyMap" in value:
-                # ...or there is a mapping
-                assert len(value["fileToKeyMap"]) > 0
-                for filename, mapped_value in value["fileToKeyMap"].items():
-                    assert mapped_value == key
-                    filename_to_look_for = filename
-            assert (host_folders["output"] / filename_to_look_for).exists()
