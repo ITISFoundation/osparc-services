1819d8d12e6ab6547df5b866aeac8605452638a8
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2019-07-14 14:26:44 +0200

    Update cc-rabbit-2d-test.py

--- services/dy-dash/cc-rabbit-2d/src/cc-rabbit-2d/cc-rabbit-2d-test.py
+++ services/dy-dash/cc-rabbit-2d/src/cc-rabbit-2d/cc-rabbit-2d-test.py
@@ -11,15 +11,26 @@ print('url_base_pathname', base_pathname)
 
 bp = Blueprint('myBlueprint', __name__, static_folder='static', template_folder='templates')
 
-@bp.route("/", methods=['GET', 'POST'])
+@bp.route('/retrieve', methods=['GET', 'POST'])
+def retrieve():
+    source = os.path.join(dirname, 'input/video.mp4')
+    dest = os.path.join(dirname, 'static/video.mp4')
+    try:
+        copyfile(source, dest)
+    except:
+        print('Failed!')
+        return serve_index()
+    print('Done!')
+    return serve_index()
+
+@bp.route('/')
 def serve_index():
-    source = "/static/video.mp4"
-    message = "CC-2D-Viewer"
+    source = '/static/video.mp4'
+    message = 'CC-2D-Viewer'
     return render_template('index.html', message=message, source=source, basepath=base_pathname)
 
 app = Flask(__name__)
 app.register_blueprint(bp, url_prefix=base_pathname)
 
 if __name__ == "__main__":
-    print("running main")
     app.run(debug=False, port=8888, host="0.0.0.0")
