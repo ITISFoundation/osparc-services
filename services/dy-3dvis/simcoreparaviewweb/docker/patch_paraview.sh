a27a860fc0ba778161022cccb147bd7cbd11ed72
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-02-24 20:32:30 +0100

    possible patch for paraview

--- services/dy-3dvis/simcoreparaviewweb/docker/patch_paraview.sh
+++ services/dy-3dvis/simcoreparaviewweb/docker/patch_paraview.sh
@@ -8,3 +8,8 @@ IFS=$'\n\t'
 # https://twisted.readthedocs.io/en/twisted-18.9.0/web/howto/web-in-60/rpy-scripts.html
 sed -i '/^from twisted.web .*/a from twisted.web import script' /opt/paraview/lib/python2.7/site-packages/wslink/server.py
 sed -i 's/^\(.*\)\(root = File(options.content).*$\)/\1root = File(options.content, ignoredExts=(".rpy",))\n\1root.processors = {".rpy": script.ResourceScript}/' /opt/paraview/lib/python2.7/site-packages/wslink/server.py
+
+# patch visualizer
+# sed -i 's|"/ws"|"'${SIMCORE_NODE_BASEPATH}'/ws"|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
+# sed -i 's|"https"===window.location.protocol|window.location.protocol.startsWith("https")|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
+# sed -i 's|"/paraview/"|"'${SIMCORE_NODE_BASEPATH}'/paraview/"|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
