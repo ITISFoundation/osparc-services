4f2bff82f9035b5b3aaf41cb5ed3134b453e3b61
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-02-25 11:06:04 +0100

    set same conditions as production

--- services/dy-3dvis/simcoreparaviewweb/docker/patch_paraview.sh
+++ services/dy-3dvis/simcoreparaviewweb/docker/patch_paraview.sh
@@ -10,6 +10,6 @@ sed -i '/^from twisted.web .*/a from twisted.web import script' /opt/paraview/li
 sed -i 's/^\(.*\)\(root = File(options.content).*$\)/\1root = File(options.content, ignoredExts=(".rpy",))\n\1root.processors = {".rpy": script.ResourceScript}/' /opt/paraview/lib/python2.7/site-packages/wslink/server.py
 
 # patch visualizer
-# sed -i 's|"/ws"|"'${SIMCORE_NODE_BASEPATH}'/ws"|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
-# sed -i 's|"https"===window.location.protocol|window.location.protocol.startsWith("https")|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
-# sed -i 's|"/paraview/"|"'${SIMCORE_NODE_BASEPATH}'/paraview/"|' /opt/paraview/share/paraview-5.6/web/visualizer/www"${SIMCORE_NODE_BASEPATH}"/Visualizer.js
+sed -i 's|"/ws"|"'${SIMCORE_NODE_BASEPATH}'/ws"|' /opt/paraview/share/paraview-5.6/web/visualizer/www/Visualizer.js
+sed -i 's|"https"===window.location.protocol|window.location.protocol.startsWith("https")|' /opt/paraview/share/paraview-5.6/web/visualizer/www/Visualizer.js
+sed -i 's|"/paraview/"|"'${SIMCORE_NODE_BASEPATH}'/paraview/"|' /opt/paraview/share/paraview-5.6/web/visualizer/www/Visualizer.js
