0e8e230e939ca700f29568f66d34c2eca0dc3550
odeimaiz <33152403+odeimaiz@users.noreply.github.com>
2020-03-23 16:55:16 +0100

    added retrieve placeholder (also avoids errors)

--- services/dy-tissue-properties/server/routes.js
+++ services/dy-tissue-properties/server/routes.js
@@ -20,7 +20,21 @@ appRouter.get('/', function (request, response) {
   response.sendFile(path.resolve(config.APP_PATH, 'index.html'));
 });
 
+appRouter.get('/retrieve', callInputRetriever);
+appRouter.post('/retrieve', callInputRetriever);
+
 module.exports = appRouter;
 
+
+function callInputRetriever(request, response) {
+  console.log('Received a call to retrieve the data on input ports from ' + request.ip);
+  const resp = {
+    "data": {
+      "size_bytes": 1
+    }
+  };
+  response.status("200").send(resp);
+  console.log("Dummy retriever");
+}
 
 module.exports.eventEmitter = eventEmitter;
