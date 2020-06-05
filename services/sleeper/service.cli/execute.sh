2fbf6b423402a146a7bec7d86f7cc5c87610ef7f
Andrei Neagu <neagu@itis.swiss>
2020-06-05 15:52:15 +0200

    removed unused code and comments

--- services/sleeper/service.cli/execute.sh
+++ services/sleeper/service.cli/execute.sh
@@ -11,18 +11,7 @@ echo   User    : "$(id "$(whoami)")"
 echo   Workdir : "$(pwd)"
 echo "..."
 echo
-# ----------------------------------------------------------------
-# This script shall be modified according to the needs in order to run the service
-# The inputs defined in ${INPUT_FOLDER}/inputs.json are available as env variables by their key in capital letters
-# For example: input_1 -> $INPUT_1
 
-# put the code to execute the service here
-# For example:
-env
-ls -al "${INPUT_FOLDER}"
-
-# then retrieve the output and move it to the $OUTPUT_FOLDER
-# as defined in the output labels
 python3 main.py
 
 
