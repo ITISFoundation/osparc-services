6af07e04d757da46841e597af2ebd67c8ac8a0ec
Andrei Neagu <neagu@itis.swiss>
2020-06-08 08:31:02 +0200

    added typing support refacted using Pathlib sleep_with_payload was improved

--- services/sleeper/src/sleeper/main.py
+++ services/sleeper/src/sleeper/main.py
@@ -3,32 +3,41 @@ import json
 import random
 import time
 import subprocess
+import logging
+
+from pathlib import Path
+from typing import Any, Optional, Callable
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-def get_from_environ(key, default=None):
+def get_from_environ(key: str, default: Any = None) -> str:
     """Returns a value from the environ if not found None"""
     return os.environ.get(key, default)
 
 
-def get_random_sleep():
+def get_random_sleep() -> int:
     """Returns a random amount of sleep between 1 and 9"""
     return random.randint(1, 9)
 
 
-def cast_bool(value):
-    """Transformas a string into a boolean"""
+def cast_bool(value: str) -> bool:
+    """Cast a probable true string value to a boolean"""
     return value.lower() in {"true", "yes", "1"}
 
 
-def validate_not_negative_int(number):
-    """Returns the number if >= 0 or a random int in range [1:9]"""
-    return number if number >= 0 else get_random_sleep()
+def ensure_sleep_policy(sleep_interval: int) -> int:
+    """If the sleep interval is negative a value 
+    in range [1:9] is returned, otherwise the original
+    value"""
+    return sleep_interval if sleep_interval >= 0 else get_random_sleep()
 
 
-def test_gpu_cuda_code():
+def test_gpu_cuda_code() -> None:
     """Dose some computation on the GPU with CUDA"""
     if get_from_environ("DISABLE_GPU_FOR_TESTING") is not None:
-        print("GPU payload disabled for testing")
+        logger.warning("GPU payload disabled for testing")
         return
 
     # if the command exists it can run on the hardware below
@@ -40,25 +49,28 @@ def test_gpu_cuda_code():
     # search the history for the CUDA implementation
 
 
-def sleep_with_payload(amount_to_sleep, target_payload=lambda: True):
+def sleep_with_payload(
+    amount_to_sleep: int, target_payload: Optional[Callable] = None
+) -> None:
     """On each interaction will run the target_payload and then sleep
     Used for validating different types of payloads based on their
     resource requirements.
     """
-    print(f"Will sleep for {amount_to_sleep} seconds")
+    logger.info("Will sleep for %s seconds", amount_to_sleep)
     for seconds in range(amount_to_sleep):
-        print(f"[PROGRESS] {seconds + 1}/{amount_to_sleep}...")
+        logger.info("[PROGRESS] %s/%s...", seconds + 1, amount_to_sleep)
 
         start = time.time()
-        target_payload()
+        if target_payload:
+            target_payload()
         # take into account the runtime of the target_payload
         time_to_sleep = max(0.0, 1.0 - (time.time() - start))
-        print(f"Remaining sleep time {time_to_sleep}")
+        logger.info("Remaining sleep time %s", time_to_sleep)
 
         time.sleep(time_to_sleep)
 
 
-def main():
+def main() -> None:
     """
     Will sleep a random amount between INPUT_1 and INPUT_2 values. If 
     these are not provided or are negative random values between 
@@ -67,39 +79,38 @@ def main():
     INPUT_3 will cause this script to fail after sleeping.
     """
 
-    file_with_int_number = get_from_environ("INPUT_1")
+    file_with_int_number = Path(get_from_environ("INPUT_1"))
     sleep_interval = int(get_from_environ("INPUT_2", get_random_sleep()))
     fail_after_sleep = cast_bool(get_from_environ("INPUT_3", "false"))
-    output_folder = get_from_environ("OUTPUT_FOLDER")
+    output_folder = Path(get_from_environ("OUTPUT_FOLDER"))
     # if this was scheduled on a node with GPU support this env variable will exist
     with_gpu_payload = get_from_environ("DOCKER_RESOURCE_VRAM") is not None
 
     sleep_from_file = get_random_sleep()
-    if os.path.isfile(file_with_int_number):
-        with open(file_with_int_number, "r") as f:
-            sleep_from_file = int(f.read().strip())
+    if file_with_int_number.is_file():
+        sleep_from_file = int(file_with_int_number.read_text().strip())
     else:
-        print(f"Could not find file '{file_with_int_number}'")
+        logger.warning("Could not find file %s", file_with_int_number)
 
     amount_to_sleep = (
-        validate_not_negative_int(sleep_interval)
-        + validate_not_negative_int(sleep_from_file)
+        ensure_sleep_policy(sleep_interval) + ensure_sleep_policy(sleep_from_file)
     ) // 2
 
     # check if there is GPU support for this container
-
+    # in the future add here more types of payloads like for MPI
     if with_gpu_payload:
         sleep_with_payload(amount_to_sleep, target_payload=test_gpu_cuda_code)
     else:
+        # sleep with no payload (CPU only)
         sleep_with_payload(amount_to_sleep)
 
     # writing program outputs
-    with open(os.path.join(output_folder, "single_number.txt"), "w") as out_file:
-        out_file.write(str(get_random_sleep()))
+    output_3_file = output_folder / "single_number.txt"
+    output_3_file.write_text(str(get_random_sleep()))
 
-    with open(os.path.join(output_folder, "outputs.json"), "w") as out_file:
-        payload = {"output_2": get_random_sleep()}
-        out_file.write(json.dumps(payload))
+    output_json_content = {"output_2": get_random_sleep()}
+    output_json = output_folder / "outputs.json"
+    output_json.write_text(json.dumps(output_json_content))
 
     # Last step should be to fail
     if fail_after_sleep:
