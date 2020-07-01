4f83508793d96bff0325df2335e07fc567bd7fc1
odeimaiz <33152403+odeimaiz@users.noreply.github.com>
2020-07-01 17:11:44 +0200

    bring back the routes

---
+++ services/dy-mapcore-widget/server/routes.js
@@ -0,0 +1,25 @@
+/* global require */
+/* global console */
+/* global module */
+/* eslint no-console: "off" */
+
+const express = require('express');
+const path = require('path');
+const appRouter = express.Router();
+const events = require('events');
+const config = require('./config');
+
+let eventEmitter = new events.EventEmitter()
+
+appRouter.use(express.json())
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+appRouter.get('/', function (request, response) {
+  console.log('Routing / to ' + path.resolve(config.APP_PATH, 'index.html'));
+  response.sendFile(path.resolve(config.APP_PATH, 'index.html'));
+});
+
+module.exports = appRouter;
+
+module.exports.eventEmitter = eventEmitter;
