74810b300c8860307081ddb4b2981b41bd24d33c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-06-02 11:10:46 +0200

    will use static-file-server

--- services/dy-static-file-server/tests/integration/conftest.py
+++ services/dy-static-file-server/tests/integration/conftest.py
@@ -17,14 +17,19 @@ def docker_client() -> docker.DockerClient:
 
 @pytest.fixture
 def docker_image_key(docker_client: docker.DockerClient) -> str:
-    image_key = "dy-httpbin:"
-    docker_images = [image for image in docker_client.images.list() if any(
-        image_key in tag for tag in image.tags)]
+    image_key = "dy-static-file-server:"
+    docker_images = [
+        image
+        for image in docker_client.images.list()
+        if any(image_key in tag for tag in image.tags)
+    ]
     return docker_images[0].tags[0]
 
 
 @pytest.fixture
-def docker_image(docker_client: docker.DockerClient, docker_image_key: str) -> docker.models.images.Image:
+def docker_image(
+    docker_client: docker.DockerClient, docker_image_key: str
+) -> docker.models.images.Image:
     docker_image = docker_client.images.get(docker_image_key)
     assert docker_image
     return docker_image
@@ -38,8 +43,7 @@ def _is_gitlab_executor() -> bool:
 def temporary_path(tmp_path: Path) -> Path:
     if _is_gitlab_executor():
         # /builds is a path that is shared between the docker in docker container and the job builder container
-        shared_path = Path(
-            "/builds/{}/tmp".format(os.environ["CI_PROJECT_PATH"]))
+        shared_path = Path("/builds/{}/tmp".format(os.environ["CI_PROJECT_PATH"]))
         shared_path.mkdir(parents=True, exist_ok=True)
         return shared_path
     return tmp_path
