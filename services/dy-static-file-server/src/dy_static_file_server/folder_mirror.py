b4d4ab6facbb41d88a91c41e5e868624f3f8fc74
Andrei Neagu <neagu@itis.swiss>
2021-09-07 14:44:05 +0200

    added folder_mirror to keep inputs in sync with outputs

---
+++ services/dy-static-file-server/src/dy_static_file_server/folder_mirror.py
@@ -0,0 +1,157 @@
+# TODO: have a watcher doing all the watching
+import hashlib
+import os
+from pdb import set_trace
+import time
+import logging
+from pathlib import Path
+from typing import Optional, List, Dict
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, DirModifiedEvent
+from threading import Thread
+
+logger = logging.getLogger(__name__)
+
+
+class UnifyingEventHandler(FileSystemEventHandler):
+    def __init__(self, input_dir: Path, output_dir: Path):
+        super().__init__()
+        self.input_dir: Path = input_dir
+        self.output_dir: Path = output_dir
+
+    def on_any_event(self, event: DirModifiedEvent):
+        super().on_any_event(event)
+        sync_directories_content(self.input_dir, self.output_dir)
+
+
+def _list_files_in_dir(path: Path) -> List[Path]:
+    return [x for x in path.rglob("*") if x.is_file()]
+
+
+def _relative_file_names_mapping(
+    containing_dir: Path, files: List[Path]
+) -> Dict[str, Path]:
+    containing_dir_str = str(containing_dir)
+
+    def _relative_file_name(file: Path) -> str:
+        return str(file).replace(containing_dir_str, "").strip("/")
+
+    return {_relative_file_name(f): f for f in files}
+
+
+def _checksum(file: Path) -> str:
+    with open(file, "rb") as f:
+        file_hash = hashlib.md5()
+        chunk = f.read(8192)
+        while chunk:
+            file_hash.update(chunk)
+            chunk = f.read(8192)
+
+    return file_hash.hexdigest()
+
+
+def _chunked_copy(input_file: Path, output_file: Path, chunk_size: int = 8192) -> None:
+    # ensure output destination diretory exists
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(input_file, "rb") as in_f, open(output_file, "wb") as out_f:
+        chunk = in_f.read(chunk_size)
+        while chunk:
+            out_f.write(chunk)
+            chunk = in_f.read(chunk_size)
+
+
+def sync_directories_content(input_dir: Path, output_dir: Path) -> None:
+    files_in_input_dir = _list_files_in_dir(input_dir)
+    files_in_output_dir = _list_files_in_dir(output_dir)
+
+    input_relative_mapping = _relative_file_names_mapping(input_dir, files_in_input_dir)
+    output_relative_mapping = _relative_file_names_mapping(
+        output_dir, files_in_output_dir
+    )
+
+    for file_name in input_relative_mapping:
+        input_file = input_relative_mapping[file_name]
+        # where to expect the output file
+        expected_output_file = output_dir / file_name
+
+        if input_file not in output_relative_mapping:
+            _chunked_copy(input_file, expected_output_file)
+            continue
+
+        if _checksum(input_file) == _checksum(expected_output_file):
+            _chunked_copy(input_file, expected_output_file)
+            continue
+
+
+def get_path_from_env(env_var_name: str) -> Path:
+    str_path = os.environ.get(env_var_name, "")
+    if len(str_path) == 0:
+        raise ValueError(f"{env_var_name} could not be found or is empty")
+
+    path = Path(str_path)
+    if not path.is_dir():
+        raise ValueError(f"{env_var_name}={str_path} is not a valid dir path")
+    if path.exists():
+        raise ValueError(f"{env_var_name}={str_path} does not exist")
+    return path
+
+
+class FolderMirror:
+    def __init__(self, input_dir: Path, output_dir: Path):
+        self.input_dir: Path = input_dir
+        self.output_dir: Path = output_dir
+
+        self._keep_running = True
+        self._thread: Optional[Thread] = None
+
+    def _runner(self) -> None:
+        event_handler = UnifyingEventHandler(
+            input_dir=self.input_dir, output_dir=self.output_dir
+        )
+        observer = Observer()
+        observer.schedule(event_handler, str(self.input_dir), recursive=True)
+        observer.start()
+        try:
+            logger.info("FolderMonitor started")
+            while self._keep_running:
+                time.sleep(0.5)
+        finally:
+            observer.stop()
+            observer.join()
+        logger.info("FolderMonitor stopped")
+
+    def start(self) -> None:
+        self._keep_running = True
+        self._thread = Thread(target=self._runner, daemon=True)
+        self._thread.start()
+
+    def stop(self) -> None:
+        self._keep_running = False
+        if self._thread:
+            self._thread.join()
+            self._thread = None
+
+    def join(self) -> None:
+        if self._thread:
+            self._thread.join()
+        else:
+            raise RuntimeError(f"{self.__class__.__name__} was not started")
+
+
+def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    input_dir = get_path_from_env("INPUT_ENV_VAR_NAME")
+    output_dir = get_path_from_env("OUTPUT_ENV_VAR_NAME")
+
+    folder_monitor = FolderMirror(input_dir, output_dir)
+    folder_monitor.start()
+    folder_monitor.join()
+
+
+if __name__ == "__main__":
+    main()
---
+++ services/dy-static-file-server/tests/unit/test_dy_static_file_server_folder_mirror.py
@@ -0,0 +1,147 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+from pathlib import Path
+from threading import Thread
+from typing import Dict
+
+import sys
+from types import ModuleType
+import pytest
+import time
+import hashlib
+
+# UTILS
+
+
+def _ensure_dir(path: Path) -> Path:
+    path.mkdir(parents=True, exist_ok=True)
+    assert path.exists() is True
+    return path
+
+
+def _list_files_in_dir(path: Path) -> Dict[str, Path]:
+    containing_dir_str = str(path)
+
+    def _relative_file_name(file: Path) -> str:
+        return str(file).replace(containing_dir_str, "").strip("/")
+
+    return {_relative_file_name(f): f for f in path.rglob("*") if f.is_file()}
+
+
+def _checksum(file: Path) -> str:
+    with open(file, "rb") as f:
+        file_hash = hashlib.sha256()
+        chunk = f.read(8192)
+        while chunk:
+            file_hash.update(chunk)
+            chunk = f.read(8192)
+
+    return file_hash.hexdigest()
+
+
+def assert_same_dir_content(input_dir: Path, output_dir: Path) -> None:
+    input_files = _list_files_in_dir(input_dir)
+    output_files = _list_files_in_dir(output_dir)
+
+    assert input_files.keys() == output_files.keys()
+
+    for key in input_files.keys():
+        input_file = input_files[key]
+        output_file = output_files[key]
+
+        assert _checksum(input_file) == _checksum(output_file)
+
+
+def create_files_in_input(input_dir: Path) -> None:
+    file_one = input_dir / "first_file.txt"
+    file_one.write_text("hello_there")
+    file_one.write_text("other stuff here")
+
+    subdir = _ensure_dir(input_dir / "subdir")
+    file_two = subdir / "second_file.txt"
+    file_two.write_text("hello_there")
+    file_two.write_text("other stuff here")
+
+
+# FIXTURES
+
+
+@pytest.fixture
+def tmp_dir(tmp_path) -> Path:
+    return Path(tmp_path)
+
+
+@pytest.fixture
+def input_dir(tmp_dir) -> Path:
+    return _ensure_dir(tmp_dir / "input_dir")
+
+
+@pytest.fixture
+def output_dir(tmp_dir) -> Path:
+    return _ensure_dir(tmp_dir / "output_dir")
+
+
+@pytest.fixture
+def dy_static_file_server(src_dir: Path) -> ModuleType:
+    # allow to search for the module
+    # insert at 1, 0 is the script path (or '' in REPL)
+    sys.path.insert(1, str(src_dir))
+
+    import dy_static_file_server
+
+    return dy_static_file_server
+
+
+# TESTS
+
+
+def test_can_import_module(dy_static_file_server: ModuleType) -> None:
+    from dy_static_file_server import folder_mirror
+
+    assert type(folder_mirror) == ModuleType
+
+
+def test_folder_mirror(
+    dy_static_file_server: ModuleType, input_dir: Path, output_dir: Path
+) -> None:
+    from dy_static_file_server.folder_mirror import FolderMirror
+
+    folder_mirror = FolderMirror(input_dir, output_dir)
+
+    folder_mirror.start()
+
+    create_files_in_input(input_dir)
+
+    #  let the threads copy and sync the files
+    time.sleep(0.25)
+
+    # TODO: check same directory strucutre
+    assert_same_dir_content(input_dir, output_dir)
+
+    folder_mirror.stop()
+
+
+def test_folder_mirror_join(
+    dy_static_file_server: ModuleType, input_dir: Path, output_dir: Path
+) -> None:
+    from dy_static_file_server.folder_mirror import FolderMirror
+
+    folder_mirror = FolderMirror(input_dir, output_dir)
+
+    # raises error if not started
+    with pytest.raises(RuntimeError) as exec:  # pylint: disable=redefined-builtin
+        folder_mirror.join()
+    assert exec.value.args[0] == "FolderMirror was not started"
+
+    # can be stopped from different thread while awaiting
+    def _stop_after_delay(folder_mirror: FolderMirror) -> None:
+        time.sleep(0.1)
+        folder_mirror.stop()
+
+    th = Thread(target=_stop_after_delay, args=(folder_mirror,), daemon=True)
+    th.start()
+
+    folder_mirror.start()
+    folder_mirror.join()
+
+    th.join()
