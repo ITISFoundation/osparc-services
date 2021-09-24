eb2b46b8705bed00971afe7b5b3f7122ce0685a4
Andrei Neagu <neagu@itis.swiss>
2021-09-24 18:01:25 +0200

    added more usable index

--- services/dy-static-file-server/src/dy_static_file_server/index_html_generator.py
+++ services/dy-static-file-server/src/dy_static_file_server/index_html_generator.py
@@ -1,5 +1,14 @@
-from pathlib import Path
 import os
+from typing import List
+from textwrap import dedent
+from datetime import datetime
+from pathlib import Path
+
+
+def _get_dir_files(dir_path: Path) -> List[str]:
+    return [
+        str(x).replace(str(dir_path), "") for x in dir_path.rglob("*") if x.is_file()
+    ]
 
 
 def _get_server_root() -> Path:
@@ -11,8 +20,42 @@ def get_index_path() -> Path:
 
 
 def _get_index_content() -> str:
-    return "<h>Hello</h1>"
+    """
+    Generates index.html content.
+    - lists all the files inside SERVER_ROOT
+    - reloads every second to be updated
+    """
+
+    files = _get_dir_files(_get_server_root())
+
+    rendered_file_list = "\n".join([f'<a href="{x}">{x}</a> <br>' for x in files])
+
+    utc_time_stamp = datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")
+
+    refres_interval: int = 5
+
+    rendered_page = f"""
+    <html>
+        <head>
+            <meta http-equiv="refresh" content="1">
+        </head>
+        <body>
+            <h1>Listing files</h1> <br>
+
+            {rendered_file_list}
+            
+            <br>
+            <br>* Last recreated {utc_time_stamp}
+            <br>* Content will be created if there is a change in the ports or the status directory.
+            <br>* Page is refreshed every {refres_interval} seconds.
+        </body>
+    </html>
+    """
+    return dedent(rendered_page)
 
 
 def generate_index() -> None:
-    get_index_path().write_text(_get_index_content())
+    index_html_path = get_index_path()
+
+    index_html_path.write_text(_get_index_content())
+    print(f"Regenerated {index_html_path}")
