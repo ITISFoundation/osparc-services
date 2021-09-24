d375e0c8de434ef6e654520c831bed075758e827
Andrei Neagu <neagu@itis.swiss>
2021-09-24 17:01:18 +0200

    migrated to diffrente webserver

--- services/dy-static-file-server/docker/custom/boot.sh
+++ services/dy-static-file-server/docker/custom/boot.sh
@@ -8,29 +8,17 @@ echo   User    : "$(id "$(whoami)")"
 echo   Workdir : "$(pwd)"
 echo   Env      : "$(env)"
 
-
-if [ -n "${SIMCORE_NODE_BASEPATH+set}" ]
-then
-    echo
-    echo moving website to "${NGINX_SERVER_ROOT}${SIMCORE_NODE_BASEPATH}"...
-    echo
-
-    mkdir -p "${NGINX_SERVER_ROOT}${SIMCORE_NODE_BASEPATH}"
-    mv "${NGINX_SERVER_ROOT}"/*.txt "${NGINX_SERVER_ROOT}${SIMCORE_NODE_BASEPATH}"
-
-    echo "Nginx will serve from ${NGINX_SERVER_ROOT}${SIMCORE_NODE_BASEPATH}"
-    
-else
-    echo "Nginx will serve from ${NGINX_SERVER_ROOT}, nothing to do"
-fi
-
-
-# display workdir content
+echo "/workdir content"
 ls -lah
 
-# ensure some random data is created in /workdir
+echo "ensure some random data is created in  in /workdir/generated-data content"
 /venv/bin/python ensure_random_workdir_data.py
 
-# keep moving data from inputs to outputs
-exec /venv/bin/python inputs_to_outputs.py &
-exec nginx -g "daemon off;" 
+echo "/workdir/generated-data content"
+ls -lah /workdir/generated-data/
+
+echo "starting background inputs->ouputs transformation"
+/venv/bin/python inputs_to_outputs.py &
+
+echo "booting static-web-server"
+exec static-web-server
